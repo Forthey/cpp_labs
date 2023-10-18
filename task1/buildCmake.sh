23d92d64d3345053e3c6cb5c1bdee382f36f4eb1
Forthey <0forthey0@gmail.com>
2023-10-19 00:52:11 +0300

    ...

--- task1/buildCmake.sh
+++ task1/buildCmake.sh
@@ -1,1 +1,6 @@
-((mkdir cmake_build || echo "cmake_build exists skipping...")  && cd cmake_build && cmake .. && make && ./ads_1) || exit
+#! /bin/sh
+project_name=$1 || (echo "no project name..." && exit)
+echo $project_name
+rm -rf cmake_build || echo "hii"
+mkdir cmake_build
+cd cmake_build && cmake .. && make && ./$project_name
