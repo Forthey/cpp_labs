a861a2bbbc5fbf3145cf814447239b2536d64f73
Forthey <0forthey0@gmail.com>
2023-12-14 21:18:00 +0300

    ...

---
+++ task5_1/source/console.hpp
@@ -0,0 +1,23 @@
+#ifndef TASK4_10_CONSOLE_H
+#define TASK4_10_CONSOLE_H
+#include <iostream>
+
+class Console {
+public:
+    typedef enum {
+        Exit = 0,
+        Load,
+        Cons,
+        Display,
+        Tree,
+        Help,
+        Hz
+    } Answer;
+
+    static std::string request(const std::string &what);
+    static void print(const std::string &what);
+    static Answer parseAnswer(const std::string &answer);
+    static std::string readLine();
+};
+
+#endif //TASK4_10_CONSOLE_H
