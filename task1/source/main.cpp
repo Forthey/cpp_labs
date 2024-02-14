d56a36d50fddd0897eaa07cef77c2b881893685c
Forthey <0forthey0@gmail.com>
2024-02-14 13:48:34 +0300

    Add a = 0 case and Make input more stable to wrong data

--- task1/source/main.cpp
+++ task1/source/main.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
-#include <string>
+#include <Windows.h>
+
 #include "quadratic_equation.hpp"
 
 class Program {
-public:
-	void print(std::string const& what) {
+	enum Color {
+		Blue = 1,
+		GREEN,
+		CYAN,
+		RED,
+		PURPLE,
+		DARK_YELLOW,
+		DEFAULT_WHITE,
+		GRAY,
+		BRIGHT_BLUE,
+		BRIGTH_GREEN,
+		BRIGHT_CYAN,
+		BRIGHT_RED,
+		PINK,
+		YELLOW,
+		BRIGHT_WHITE,
+	};
+
+    HANDLE hConsole;
+
+	void print(std::string const& what, Color color = Color::DEFAULT_WHITE) {
+		SetConsoleTextAttribute(hConsole, color);
 		std::cout << what << std::endl;
+		SetConsoleTextAttribute(hConsole, Color::DEFAULT_WHITE);
 		std::cout << "> ";
 	}
 
+	void printProgramDescription() {
+		std::cout << 
+			"Made by Drekalov Nikita, group 5030102/20001" << std::endl <<
+			"The program counts the roots of the quadratic equation" << std::endl <<
+			"______________________________________________________" << std::endl;
+	}
+public:
+	Program() {
+		hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	}
+
 	void start() {
+		double a, b, c;
+
+		printProgramDescription();
+
 		while (true) {
 			print("Input coefficients of quadratic equation ax^2+bx+c=0 separated by space\nType '0 0 0' to end program");
-			double a, b, c;
-			std::cin >> a >> b >> c;
+			if (!(std::cin >> a >> b >> c)) {
+				print("ERROR: You should input 3 numbers separated by space to make program work", Color::RED);
+				std::cin.clear();
+				std::cin.ignore(INT_MAX, '\n');
+				continue;
+			}
 
 			if (a == 0 && b == 0 && c == 0) {
 				break;
@@ -22,11 +63,13 @@ public:
 			Alg::QuadraticEq equation(a, b, c);
 			std::vector<double> const& roots = equation.solve();
 
-			std::cout << "Equation has " << roots.size() << " real roots: ";
+			std::cout << "Equation " + equation.toString() + " has " << roots.size() << " real roots" << std::endl;
 			for (double root : roots) { 
 				std::cout << root << " ";
 			}
-			std::cout << std::endl;
+			std::cout << std::endl << std::endl;
+
+			std::cin.ignore(INT_MAX, '\n');
 		}
 	}
 };
--- task1/source/quadratic_equation.hpp
+++ task1/source/quadratic_equation.hpp
@@ -1,15 +1,20 @@
 #include <vector>
+#include <string>
 
 namespace Alg {
 
 	class QuadraticEq {
 		double const a, b, c;
 		std::vector<double> roots;
-	public:
-		QuadraticEq(double const a, double const b, double const c) : a(a), b(b), c(c) {};
 
-		std::vector<double>& const solve();
-		std::vector<double>& const getRoots();
+		void solveAsLinear();
+		void solveAsQuadratic();
+	public:
+		QuadraticEq(double const a, double const b, double const c);
+
+		std::vector<double> const & solve();
+		std::vector<double> const & getRoots() const;
+		std::string const toString() const;
 	};
 
 }
