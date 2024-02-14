5a0fb4823cd139a3c69017a275890357910b4400
Forthey <0forthey0@gmail.com>
2024-02-14 13:48:34 +0300

    Add a = 0 case and Make input more stable to wrong data

--- task1/source/quadratic_equation.cpp
+++ task1/source/quadratic_equation.cpp
@@ -1,6 +1,19 @@
 #include "quadratic_equation.hpp"
 
-std::vector<double>& const Alg::QuadraticEq::solve()
+using namespace Alg;
+
+QuadraticEq::QuadraticEq(double const a, double const b, double const c) : a(a), b(b), c(c) 
+{
+};
+
+void QuadraticEq::solveAsLinear()
+{
+	if (b != 0) {
+		roots.push_back(-c / b);
+	}
+}
+
+void Alg::QuadraticEq::solveAsQuadratic()
 {
 	double D = b * b - 4 * a * c;
 
@@ -11,11 +24,26 @@ std::vector<double>& const Alg::QuadraticEq::solve()
 	else if (D == 0) {
 		roots.push_back(-b / 2 / a);
 	}
+}
+
+std::vector<double> const & QuadraticEq::solve()
+{
+	if (a == 0) {
+		solveAsLinear();
+	}
+	else {
+		solveAsQuadratic();
+	}
 
 	return roots;
 }
 
-std::vector<double>& const Alg::QuadraticEq::getRoots()
+std::vector<double> const & QuadraticEq::getRoots() const
 {
 	return roots;
+}
+
+std::string const QuadraticEq::toString() const
+{
+	return std::to_string(a) + "*x^2 + " + std::to_string(b) + "*x + " + std::to_string(c) + " = 0";
 }
